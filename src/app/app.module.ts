109ac8d42f60742bbbc72713cde048f3375325f5
Omar Sayed <orhman@hotmail.com>
2022-08-06 22:28:13 +0200

    Add game board module & score component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameBoardComponent } from './features/game-board/game-board.component';
+import { ScoreComponent } from './features/gameItems/score/score.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameBoardComponent,
+    ScoreComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/features/game-board/game-board.module.ts
@@ -0,0 +1,13 @@
+import { ScoreComponent } from './../gameItems/score/score.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [ScoreComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GameBoardModule { }
