ebbb48ddf9b17d06eb2579845b0a73757963c309
Omar Sayed <orhman@hotmail.com>
2022-08-11 08:39:33 +0200

    Add PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { GameBoardModule } from './features/game-board/game-board.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
   
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, GameBoardModule],
+  imports: [BrowserModule, GameBoardModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [],
   bootstrap: [AppComponent],
 })
